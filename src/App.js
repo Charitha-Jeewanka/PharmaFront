b613ec0cbf9d3c4784cafe419ee8faf7f71f647e
Ketawala <lakshanisuru170@gmail.com>
2021-07-26 00:46:46 +0530

    commit message

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Login from "./Components/Login";
 import Signin from "./Components/Signin";
+import About from "./Components/about";
 import { BrowserRouter as Router, Route ,Switch} from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Route>
-        <Signin/>
+        <About/>
       </Route>
     </Router>
   );
